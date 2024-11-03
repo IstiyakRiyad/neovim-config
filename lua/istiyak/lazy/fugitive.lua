dc2b404e575658342c5c5ac14634eae045f82ae3
Istiyak Hossain <istiyak.hossain@technonext.com>
2024-11-03 22:36:58 +0600

    Updating lua

--- lua/istiyak/lazy/fugitive.lua
+++ lua/istiyak/lazy/fugitive.lua
@@ -3,7 +3,7 @@ return {
 
 
 	config = function()
-		vim.keymapas.set("n", "<leader>gs", vim.cmd.Git);
+		vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
 
 		-- local Fugitive = vim.api.nvim_create_augroup("Istiyak_Fugitive", {})
 		--
